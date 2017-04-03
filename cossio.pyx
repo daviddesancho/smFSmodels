14f855eb4606d3e727f6411e354941fa53f61202
daviddesancho <daviddesancho@gmail.com>
2017-04-03 10:24:32 +0200

    reshuffling variables

--- cossio.pyx
+++ cossio.pyx
@@ -9,13 +9,28 @@ cdef float beta = 1.0
 def Gx(float x, float barrier=5.):
     """
     Functional form of the molecular free energy surface
+
+    x : float
+        Value of molecular coordinate x.
+
+    barrier : float
+        Value of free energy barrier on molecular coordinate.
     
     """
     return barrier*f(x)
 
-def V(float q, float x, float kl):
+def V(float q, float x, float kl=0.):
     """
     Harmonic contribution from the instrument
+
+    q : float
+        Value of measured coordinate q.
+
+    x : float
+        Value of molecular coordinate x.
+
+    kl : float
+        Spring constant of harmonic potential.
 
     """
     return 0.5*kl*(q - x)**2
@@ -24,12 +39,27 @@ def Gqx(float q, float x, float barrier=5.0, float kl=0.):
     """
     Functional form of the 2D free energy surface
 
+    q : float
+        Value of measured coordinate q.
+
+    x : float
+        Value of molecular coordinate x.
+
+    barrier : float
+        Value of free energy barrier on molecular coordinate.
+    
+    kl : float
+        Spring constant of harmonic potential.
+
     """
     return Gx(x, barrier) + V(q, x, kl)
 
 def f(float x):
     """
     Functional form of the bistable potential
+
+    x : float
+        Value of molecular coordinate x.
 
     """
     cdef float xxd
@@ -43,6 +73,9 @@ def f(float x):
 def df(float x):
     """
     Derivative of the bistable potential
+
+    x : float
+        Value of molecular coordinate x.
 
     """
     cdef float xxd
@@ -58,9 +91,18 @@ def df(float x):
 #    else:
 #        return 4*x*(np.abs(x)-1)/np.abs(x) 
  
-def dVdx(float q, float x, float kl):
+def dVdx(float q, float x, float kl=0.):
     """
     Derivative of instrumental contribution with respect to x
+
+    q : float
+        Value of measured coordinate q.
+
+    x : float
+        Value of molecular coordinate x.
+
+    kl : float
+        Spring constant of harmonic potential.
 
     """
     return -kl*(q - x)
@@ -69,6 +111,15 @@ def dGqxdq(float q, float x, float kl=0.):
     """
     Derivative of the 2D free energy surface with respect to q
 
+    q : float
+        Value of measured coordinate q.
+
+    x : float
+        Value of molecular coordinate x.
+
+    kl : float
+        Spring constant of harmonic potential.
+
     """
     return kl*(q - x)
 
@@ -76,24 +127,75 @@ def dGqxdx(float q, float x, float barrier=5., float kl=0.):
     """
     Derivative of the 2D free energy surface with respect to x
 
+    q : float
+        Value of measured coordinate q.
+
+    x : float
+        Value of molecular coordinate x.
+
+    barrier : float
+        Value of free energy barrier on molecular coordinate.
+    
+    kl : float
+        Spring constant of harmonic potential.
+
     """
     return barrier*df(x) + dVdx(q, x, kl)
 
-def delta_q_eff(float q, float x, float dt, float kl, float rg,\
-        float Dq, float sqrt2Dqdt):
+def delta_q_eff(float q, float x, float bDqdt, float sqrt2Dqdt, \
+        float rg, float kl=0.):
     """
     Displacement in q
 
-    """
-    return -dt*Dq*beta*dGqxdq(q,x,kl) + sqrt2Dqdt*rg
+    q : float
+        Value of measured coordinate q.
 
-def delta_x_eff(float q, float x, float dt, float kl, float rg, \
-        float Dx, float sqrt2Dxdt):
+    x : float
+        Value of molecular coordinate x.
+
+    kl : float
+        Spring constant of harmonic potential.
+
+    rg : float
+        Normally distributed random number.
+
+    bDxDt : float
+        beta*Dx*dt
+
+    sqrt2Dqdt : float
+        sqrt(2*Dq*dt)
+
+    """
+    return -bDqdt*dGqxdq(q,x,kl) + sqrt2Dqdt*rg
+
+def delta_x_eff(float q, float x, \
+        float bDxdt, float sqrt2Dxdt, float rg, float kl=0.):
     """
     Displacement in x
 
+    q : float
+        Value of measured coordinate q.
+
+    x : float
+        Value of molecular coordinate x.
+
+    barrier : float
+        Value of free energy barrier on molecular coordinate.
+    
+    kl : float
+        Spring constant of harmonic potential.
+
+    rg : float
+        Normally distributed random number.
+
+    bDxdt : float
+        beta*Dx*dt
+
+    sqrt2Dxdt : float
+        sqrt(2*Dx*dt)
+
     """
-    return -dt*Dx*beta*dGqxdx(q,x,kl) + sqrt2Dxdt*rg 
+    return -bDxdt*dGqxdx(q,x,kl) + sqrt2Dxdt*rg 
 
 def run_brownian(float x0=5., float q0=0., float Dx=0., float Dq=0.,\
         float kl=0., float dt=1.e-3, int numsteps=100000):
@@ -128,7 +230,10 @@ def run_brownian(float x0=5., float q0=0., float Dx=0., float Dq=0.,\
     cdef int k
     cdef float x, q
     cdef float sqrt2Dxdt, sqrt2Dqdt
+    cdef float bDqdt, bDxdt    
 
+    bDqdt = beta*Dq*dt
+    bDxdt = beta*Dx*dt
     sqrt2Dxdt = np.sqrt(2.*Dx*dt)
     sqrt2Dqdt = np.sqrt(2.*Dq*dt)
 
@@ -140,8 +245,8 @@ def run_brownian(float x0=5., float q0=0., float Dx=0., float Dq=0.,\
     xk = [x]
     qk = [q]
     while True:
-        x += delta_x_eff(q, x, dt, kl, rgaussx[k], Dx, sqrt2Dxdt)
-        q += delta_q_eff(q, x, dt, kl, rgaussq[k], Dq, sqrt2Dqdt)
+        x += delta_x_eff(q, x, bDxdt, sqrt2Dxdt, rgaussx[k], kl)
+        q += delta_q_eff(q, x, bDqdt, sqrt2Dqdt, rgaussq[k], kl)
         k +=1
         if k%100 == 0:
             xk.append(x)
